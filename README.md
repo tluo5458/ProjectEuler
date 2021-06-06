317735b7065656888f58e53835975fa19ed218bb
Thomas Luo <tluo5458@gmail.com>
2021-06-06 12:11:15 -0400

    Problem 169

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # ProjectEuler
 ProjectEuler Code Solutions. The problems can be found at projecteuler.net.
 
-Currently completed problems are 1-146, 148-149, 151, 157, 162, 164, 173-174, 179, 183, 187-188, 190-191, 197, 206, 243, 719, 727, and 743. Any files that aren't in here were done by hand or otherwise not in code.
+Currently completed problems are 1-146, 148-149, 151, 157, 162, 164, 169, 173-174, 179, 183, 187-188, 190-191, 197, 206, 243, 719, 727, and 743. Any files that aren't in here were done by hand or otherwise not in code.
 
 Currently notable:
 - All 1-100 completed
-- All problems 101-200 below 50% difficulty completed 
+- All problems 101-200 below 55% difficulty except for problem 166 completed 
 
 A lot of the earlier files (particularly problems 1-50) are very messily written: it took a while for me to really start caring about how these were formatted since I wasn't intending on making these public. Also some of them are very inefficient. I generally try to follow a 3-second rule in terms of program runtime, but this has only really applied to more recent files (problems 100+), and even then there are definitely exceptions.
 
--- src/problems1xx/Problem169.java
+++ src/problems1xx/Problem169.java
@@ -1,47 +1,50 @@
 package problems1xx;
 
 import java.math.BigInteger;
-import java.util.TreeSet;
 
 import commonMethods.Timing;
 
 public class Problem169 {
-	// n is the binary representation of the number
-	public static int f(String n) {
-		TreeSet<String> done = new TreeSet<String>();
-		TreeSet<String> bins = new TreeSet<String>();
-		bins.add(n);
-		while (!bins.isEmpty()) {
-			char[] bin = bins.first().toCharArray();
-			done.add(bins.first());
-			bins.remove(bins.first());
-			for (int i = 0; i < bin.length - 1; i++) {
-				if (bin[i] > '0' && bin[i + 1] == '0') {
-					char[] dupe = bin.clone();
-					dupe[i] -= 1;
-					dupe[i + 1] += 2;
-					String s = String.valueOf(dupe);
-					if (s.charAt(0) == '0') {
-						s = s.substring(1);
-					}
-					if (!done.contains(s)) {
-						bins.add(s);
-					}
-				}
+	// n is the binary representation of the number; assume n > 0
+	// dynamic programming solution, iterates through the digits one by one
+	public static long f(String n) {
+		// zero is the number of solutions so far where the rightmost non-one digit is a 0
+		long zero = 1;
+		// two is the same thing but rightmost non-one digit is a 2
+		long two = 0;
+		// numOnes is the number of ones at the end of each solution
+		int numOnes = 1;
+		for (int i : n.substring(1).toCharArray()) {
+			if (i == 48) {
+				// if next digit is 0
+				// ind1 is temporary variable to hold zero; in this case the next zero is zero + two because these are just 0 appended to all of the previous sols
+				long ind1 = zero + two;
+				// two is (numOnes * zero) + (numOnes + 1) * two because for each 1 at the end, we can replace the 10 with 02 and repeat until we reach 0
+				// there's an extra two because once we reach that 2, it'll go 20 -> 11
+				// for example, 0110 -> 0110, 0102, 0022 so it contributes 2 to two (and 1 to zero)
+				// but 2110 -> 2110, 2102, 2022, 1222 so it contributes 3 to two (and 1 to zero)
+				two = two + numOnes * (zero + two);
+				zero = ind1;
+				numOnes = 0;
+			} else if (i == 49) {
+				// if next digit is 1
+				// all solutions will just be a 1 appended to the end of each of the previous solutions
+				numOnes++;
 			}
 		}
-		return done.size();
+		return zero + two;
 	}
 	
-	public static int fPow10(int exp) {
+	public static long fPow10(int exp) {
+		// BigInteger.TEN.pow(exp) = 10^exp, .toString(2) gets the binary representation
 		return f(BigInteger.TEN.pow(exp).toString(2));
 	}
 	
 	public static void main(String[] args) {
 		Timing t = new Timing();
 		t.start();
-		System.out.println(fPow10(12));
-		// true answer would be fPow10(75), but 12 is already taking 6 seconds lol
+		// under 1ms yay
+		System.out.println(fPow10(25));
 		t.end();
 	}
 }
